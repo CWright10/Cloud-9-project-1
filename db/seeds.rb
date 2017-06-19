217d529ca63ccbdcaf7b3148cb6628222008f41c
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 17:08:48 +0000

    Added Portfolio items via Resource Generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Had a alright day in coding class today"
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "I know how to blog #{skill}",
+    percent_utilized: 15
+    
+    )
+end 
+
+puts "5 skill posts created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "I mowed the lawn at my house and gave my little brother money" ,
+    main_image: "http://placehold.it.com/600x400" ,
+    thumb_image: "http://placehold.it.com/350x200"
+   )
+end
+
+puts "9 portfolio items created"
