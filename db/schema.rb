35cc46b463bb0ad733fbf9c258945ef9898b51e7
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:31:55 +0000

    Updated status for enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621144540) do
+ActiveRecord::Schema.define(version: 20170621153156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
