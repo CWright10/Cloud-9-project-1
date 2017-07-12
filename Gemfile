82bdd5d18a74f43f375d8eb13e59676b0c7676c4
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 16:38:07 +0000

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.3'
+
+gem 'wright_view_tool', git: "https://github.com/CWright10/wright_view_tool" 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    WrightViewTool::Renderer.copyright 'Caden Wright', 'All rights reserved'
+  end
 end
