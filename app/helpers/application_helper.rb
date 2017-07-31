3630b7b6b576d07f86d7d7543e515f3df1a4259f
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 14:13:40 +0000

    Did some stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def login_helper style = ''
-    if current_user.is_a?(GuestUser)
+    if current_user.is_a?(User)
       link_to "Logout", destroy_user_session_path, method: :delete, class: style
     else
       (link_to "Register", new_user_registration_path, class: style) +
