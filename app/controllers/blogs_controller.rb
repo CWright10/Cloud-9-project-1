b9788d3a30c90e7d5232c8ba12466d7f46a46c6f
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 15:47:24 +0000

    Fixed stupid mistakes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,13 +18,13 @@ class BlogsController < ApplicationController
   # GET /blogs/1.json
   def show
     if logged_in?(:site_admin) || @blog.published?
-        @blog = Blog.includes(:comments).friendly.find(params[:id])
-        @comment = Comment.new
-  
-        @page_title = @blog.title
-        @seo_keywords = @blog.body
-      else
-        redirect_to blogs_path, notice: "You are not authorized to access this page!"
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+
+      @page_title = @blog.title
+      @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
     end
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper style = ''
-    if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
-    else
+    if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
@@ -70,6 +70,5 @@ module ApplicationHelper
   def alert_generator msg
     js add_gritter(msg, title: "Caden Wright Portfolio", sticky: false)
   end
-
 end
 
